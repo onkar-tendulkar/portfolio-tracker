f8de141cd7a522c1b26a321e279bce7a2af2135d
Onkar <onkar.milind.tendulkar@gmail.com>
2019-09-26 23:49:04 +0530

    Allow cross origin request for development. https://howtodoinjava.com/spring5/webmvc/spring-mvc-cors-configuration/

--- src/main/java/com/onkar/web/PortfolioController.java
+++ src/main/java/com/onkar/web/PortfolioController.java
@@ -9,13 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @Controller
 public class PortfolioController {
 
